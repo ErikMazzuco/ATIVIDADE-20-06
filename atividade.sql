67030afbfebecf4f36813b50de8e7f4f9921c27e
Erik Mazzuco <erikmazzucomatheus789@gmail.com>
2022-09-18 17:23:22 -0300

    Umas paradas

--- atividade.sql
+++ atividade.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 18-Set-2022 às 21:16
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 7.4.27
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+--criaçao da tabela
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `atividade`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `funcionarios`
---
 
 CREATE TABLE `funcionarios` (
   `id` int(11) NOT NULL,
@@ -36,34 +11,3 @@ CREATE TABLE `funcionarios` (
   `dataout` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Extraindo dados da tabela `funcionarios`
---
-
-INSERT INTO `funcionarios` (`id`, `nome`, `endereco`, `cargo`, `datain`, `dataout`) VALUES
-(1, 'ERIK', '', '25', '0000-00-00', '0000-00-00');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `funcionarios`
---
-ALTER TABLE `funcionarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `funcionarios`
---
-ALTER TABLE `funcionarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
